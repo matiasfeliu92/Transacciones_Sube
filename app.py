d9fd740a3cd4918e471287ab68978e70a99a7436
matiasfeliu92 <matumazparrote@gmail.com>
2023-10-29 19:02:50 -0300

    delete iplot code line in app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import psycopg2
 from sqlalchemy import create_engine
 import os
 import plotly.express as px
-from plotly.offline import init_notebook_mode, iplot
 from dotenv import load_dotenv
 
 load_dotenv()
-init_notebook_mode(connected=True)
 
 database_url = os.getenv('DATABASE_URL')
 engine = create_engine(database_url)
