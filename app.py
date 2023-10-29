4b78ddf64ea06ba4bbcb17263497bde426068451
matiasfeliu92 <matumazparrote@gmail.com>
2023-10-29 19:08:36 -0300

    update database_url env in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-database_url = os.getenv('DATABASE_URL')
+database_url = st.secrets["DATABASE_URL"]
 engine = create_engine(database_url)
 
 query = """
